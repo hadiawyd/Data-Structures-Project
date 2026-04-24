699b55d50a68c58892fd670951291f14bbe50615
waniakanwal25-rgb <waniakanwal25@gmail.com>
2026-04-25 01:42:45 +0500

    Add Node.h

--- code/Node.h
+++ code/Node.h
@@ -1,1 +1,17 @@
+#ifndef NODE_H
+#define NODE_H
 
+template <typename T>
+struct Node
+{
+    T data;
+    Node<T>* next;
+
+    Node(T val)
+    {
+        data = val;
+        next = nullptr;
+    }
+};
+
+#endif
