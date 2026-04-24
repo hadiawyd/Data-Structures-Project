f52ba2315e342800de6b173f7cafff8e5bcb034f
waniakanwal25-rgb <waniakanwal25@gmail.com>
2026-04-25 01:46:35 +0500

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "MySTL.h"
 
+using namespace std;
+
+int main()
+{
+    MyStack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+
+    MyQueue<int> q;
+    q.enqueue(11);
+    q.enqueue(12);
+    q.enqueue(13);
+
+    cout << q.front() << endl;
+    q.dequeue();
+    cout << q.front() << endl;
+
+    return 0;
+}
