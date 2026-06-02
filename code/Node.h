723abb6177888dcee4ce2c5f60d9b2f10682b89b
hadiawyd <Hadia.Sajid.31687@khi.iba.edu.pk>
2026-06-02 22:51:04 +0500

    Delete Node.h and its contents
    Removed the Node template structure and header guards.

--- code/Node.h
+++ code/Node.h
@@ -1,17 +1,1 @@
-#ifndef NODE_H
-#define NODE_H
 
-template <typename T>
-struct Node
-{
-    T data;
-    Node<T>* next;
-
-    Node(T val)
-    {
-        data = val;
-        next = nullptr;
-    }
-};
-
-#endif
