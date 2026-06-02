9169e92a855859f24f8f5c38f44a379ae978d4e9
hadiawyd <Hadia.Sajid.31687@khi.iba.edu.pk>
2026-06-02 22:46:03 +0500

    Rename header file and update includes

--- code/MySTL.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYSTL_H
-#define MYSTL_H
-
-#include "MyStack.h"
-#include "MyQueue.h"
-#include "LinkedList.h"
-#include "Vector.h"
-#endif
---
+++ code/STL.h
@@ -0,0 +1,10 @@
+#ifndef STL_H
+#define STL_H
+
+#include "Stack.h"
+#include "Queue.h"
+#include "LinkedList.h"
+#include "Vector.h"
+#include "BST.h"
+#include "HashMap.h"
+#endif
